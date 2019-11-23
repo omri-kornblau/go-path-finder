898bcd81be7205f33cad4a904bc4ea94b61ff366
Omri Kornblau <omri.354@gmail.com>
2019-11-23 17:56:28 +0200

    Add utils

---
+++ spline/utils.go
@@ -0,0 +1,27 @@
+package spline
+
+import (
+	"math"
+)
+
+func calcPolynom(factors []float64, s float64) float64 {
+	var result float64 = 0
+	for index, factor := range factors {
+		result += factor * math.Pow(s, float64(index))
+	}
+
+	return result
+}
+
+func getDerivativeFactors(factors []float64) []float64 {
+	var newFactors []float64
+	for index, factor := range factors[1:] {
+		newFactors[index] = factor * float64(index)
+	}
+
+	return newFactors
+}
+
+func PointsDistance(first, second Point) float64 {
+	return math.Sqrt(math.Pow(first.Y-second.Y, 2) + math.Pow(first.X-second.X, 2))
+}
