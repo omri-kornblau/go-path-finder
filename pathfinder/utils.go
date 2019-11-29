7a9a2f1e6bfaee8776872e95cc06392ec8598dd4
Omri Kornblau <omri.354@gmail.com>
2019-11-29 14:45:48 +0200

    Add pathfinder angle diff util

--- pathfinder/utils.go
+++ pathfinder/utils.go
@@ -1,5 +1,7 @@
 package pathfinder
 
+import "math"
+
 func Sign(num float64) float64 {
 	if num > 0 {
 		return 1
@@ -7,4 +9,12 @@ func Sign(num float64) float64 {
 		return -1
 	}
 	return 0
+}
+
+func AngleDiff(firstAngle, secondAngle float64) float64 {
+	diff := secondAngle - firstAngle
+	for diff < -2*math.Pi || diff > 2*math.Pi {
+		diff += 2*math.Pi - Sign(diff)
+	}
+	return diff
 }
